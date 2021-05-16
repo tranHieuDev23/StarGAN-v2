978e8f25778cbb2649b22dc350eb724dd412c36d
Tran Minh Hieu <tranhieu23.code@outlook.com>
2021-05-17 00:53:54 +0700

    Implement patchgan

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,31 +1,29 @@
 import torch
 import torch.nn as nn
-import numpy as np
-from models.blocks import ResidualBlock
 
 
 class Discriminator(nn.Module):
-    def __init__(self, img_size=256, num_domains=2, max_conv_dim=512):
+    def __init__(self, img_size=256, num_domains=2, d=64):
         super().__init__()
-        dim_in = 2**14 // img_size
-        layers = []
-        layers.append(nn.Conv2d(3, dim_in, 1, 1, 0))
-
-        repeat_num = int(np.log2(img_size)) - 2
-        for _ in range(repeat_num):
-            dim_out = min(dim_in * 2, max_conv_dim)
-            layers.append(ResidualBlock(dim_in, dim_out, downsample=True))
-            dim_in = dim_out
-
-        layers.append(nn.LeakyReLU(0.2))
-        layers.append(nn.Conv2d(dim_out, dim_out, 4, 1, 0))
-        layers.append(nn.LeakyReLU(0.2))
-        layers.append(nn.Conv2d(dim_out, num_domains, 1, 1, 0))
-        self.main = nn.Sequential(*layers)
+        self.main = nn.Sequential(
+            nn.Conv2d(3, d, 4, 2, 1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(d, d * 2, 4, 2, 1),
+            nn.BatchNorm2d(d * 2),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(d * 2, d * 4, 4, 2, 1),
+            nn.BatchNorm2d(d * 4),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(d * 4, d * 8, 4, 1, 1),
+            nn.BatchNorm2d(d * 8),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(d * 8, num_domains, 4, 1, 1)
+        )
 
     def forward(self, x, y):
         output = self.main(x)
-        output = output.view(output.size(0), -1)
         idx = torch.LongTensor(range(y.size(0))).to(y.device)
         output = output[idx, y]
+        output = output.view(output.size(0), -1)
+        output = torch.mean(output, dim=1)[0]
         return output
